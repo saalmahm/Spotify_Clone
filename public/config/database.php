07486d8f39a409d31fffd51dfdb310c389b46add
salma hamdi <saalmahm51@gmail.com>
2025-02-09 23:35:40 +0100

    La liaison avec la base de donner postgreSQL

---
+++ public/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "musify_clone";
+    private $username = "postgres";
+    private $password = "salmahm";
+    private $conn;
+
+    public function connect() {
+        try {
+            $this->conn = new PDO("pgsql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
