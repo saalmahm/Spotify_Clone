4323b6923b5c10571129869295917cfddd9d4abd
salma hamdi <saalmahm51@gmail.com>
2025-02-09 23:34:00 +0100

    Crée le fichier responsable des routes pour des différents page avec l'appel de la méthod précis dans un controller

---
+++ app/routes/router.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../controllers/ArtisteController.php';
+
+class Router {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function run() {
+        $requestUri = parse_url($_GET['route'], PHP_URL_PATH);
+        $requestUri = trim($requestUri, '/');
+    
+        // Routes par défaut
+        $routes = [
+            'home' => ['homeController', 'show'],
+            'login' => ['UserController', 'login'],
+            'profile' => ['UserController', 'updateProfile'],
+            'register' => ['UserController', 'register'],
+            'uploadSong' => ['ArtisteController', 'uploadSong'],
+            'uploadAlbum'=> ['ArtisteController', 'organiserChansons']
+        ];
+    
+        // Vérification de la route et appel du contrôleur
+        if (isset($routes[$requestUri])) {
+            list($controller, $action) = $routes[$requestUri];
+    
+            // Vérification si l'utilisateur est authentifié pour certaines routes
+            if ($requestUri !== 'login' && $requestUri !== 'register' && !isset($_SESSION['user'])) {
+                header('Location: login');
+                exit();
+            }
+            $controllerInstance = new $controller($this->db);
+            call_user_func([$controllerInstance, $action]);
+           
+        }
+    }
+    
+}
